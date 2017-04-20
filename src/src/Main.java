0653d3ffafde3d25b7833bb077afa10299c17d68
jurdy <stephencjurd@gmail.com>
2017-04-20 13:09:25 -0500

    stephen added a test line
    i added a test line to do stuff

--- src/src/Main.java
+++ src/src/Main.java
@@ -1,5 +1,6 @@
 public class Main {
     public static void main(String[] args){
         System.out.println("Getting it setup yo");
+        System.out.println("stephen jurds own little line +3");
     }
 }
