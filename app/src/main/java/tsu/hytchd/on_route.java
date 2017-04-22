795816af64b5dc6af63ddde777472fd4da74d82a
Hajjhajj <armando.soriano@go.tarleton.edu>
2017-04-22 16:11:18 -0500

    Added Java classes for xmls

---
+++ app/src/main/java/tsu/hytchd/on_route.java
@@ -0,0 +1,13 @@
+package tsu.hytchd;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class on_route extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.add_org_n_dest);
+    }
+}
