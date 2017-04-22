0d0c672c1157083ee0390bd879bf45bd47aea3df
Brandon Jurado <brandonjurado13@yahoo.com>
2017-04-22 14:03:33 -0500

    Success reroute

---
+++ app/src/main/java/tsu/hytchd/add_org_n_dest.java
@@ -0,0 +1,13 @@
+package tsu.hytchd;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class add_org_n_dest extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.add_org_n_dest);
+    }
+}
