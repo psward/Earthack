87a900720350d1e235f5bad95ce9c9b52edfba41
psward <prestonsward@gmail.com>
2017-04-22 18:45:45 -0500

    That marker though

--- app/src/main/java/tsu/hytchd/add_org_n_dest.java
+++ app/src/main/java/tsu/hytchd/add_org_n_dest.java
@@ -1,7 +1,16 @@
 package tsu.hytchd;
 
+import android.content.Context;
+import android.content.Intent;
+import android.location.Address;
+import android.location.Criteria;
+import android.location.Geocoder;
 import android.location.Location;
+import android.location.LocationManager;
 import android.support.annotation.NonNull;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
@@ -18,6 +27,11 @@ import android.os.Bundle;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
+
+import org.apache.commons.lang3.StringUtils;
+
+import java.io.IOException;
+import java.util.List;
 
 import static tsu.hytchd.R.id.map;
 
@@ -43,6 +57,8 @@ public class add_org_n_dest extends AppCompatActivity
     MarkerOptions mp = new MarkerOptions();
     private GoogleMap mMap;
 
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -53,6 +69,18 @@ public class add_org_n_dest extends AppCompatActivity
         mapFragment.getMapAsync(this);
 
         mp.position(new LatLng(location.getLatitude(), location.getLongitude()));
+
+        Button addRoute = (Button) findViewById(R.id.button2);
+
+
+        addRoute.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        onMapSearch(v);
+                    }
+                }
+        );
 
     }
 
@@ -72,7 +100,7 @@ public class add_org_n_dest extends AppCompatActivity
             // Permission to access the location is missing.
             PermissionUtils.requestPermission(this, LOCATION_PERMISSION_REQUEST_CODE,
                     Manifest.permission.ACCESS_FINE_LOCATION, true);
-        } else if (mMap != null) {
+        } else if (mMap != null  && isLocationServiceEnabled()) {
             mMap.setMyLocationEnabled(true);
             mMap.clear();
         }
@@ -124,6 +152,33 @@ public class add_org_n_dest extends AppCompatActivity
     private void showMissingPermissionError() {
         PermissionUtils.PermissionDeniedDialog
                 .newInstance(true).show(getSupportFragmentManager(), "dialog");
+    }
+
+    public boolean isLocationServiceEnabled() {
+        LocationManager lm = (LocationManager)
+                this.getSystemService(Context.LOCATION_SERVICE);
+        String provider = lm.getBestProvider(new Criteria(), true);
+        return (StringUtils.isNotBlank(provider) &&
+                !LocationManager.PASSIVE_PROVIDER.equals(provider));
+    }
+    public void onMapSearch(View view) {
+        EditText locationSearch = (EditText) findViewById(R.id.textView4);
+        String location = locationSearch.getText().toString();
+        List<Address>addressList = null;
+
+        if (location != null || !location.equals("")) {
+            Geocoder geocoder = new Geocoder(this);
+            try {
+                addressList = geocoder.getFromLocationName(location, 1);
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            Address address = addressList.get(0);
+            LatLng latLng = new LatLng(address.getLatitude(), address.getLongitude());
+            mMap.addMarker(new MarkerOptions().position(latLng).title("Marker"));
+            mMap.animateCamera(CameraUpdateFactory.newLatLng(latLng));
+        }
     }
 
 }
