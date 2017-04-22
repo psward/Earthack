2fad21acd2869333c473be410cbfc18a572cf1bd
Brandon Jurado <brandonjurado13@yahoo.com>
2017-04-22 16:29:36 -0500

    Conflicts fixed

--- app/src/main/java/tsu/hytchd/MapsActivity.java
+++ app/src/main/java/tsu/hytchd/MapsActivity.java
@@ -6,10 +6,10 @@ import android.widget.Toast;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
-        import com.google.android.gms.maps.GoogleMap.OnMyLocationButtonClickListener;
+import com.google.android.gms.maps.GoogleMap.OnMyLocationButtonClickListener;
 import com.google.android.gms.maps.LocationSource;
 import com.google.android.gms.maps.OnMapReadyCallback;
-        import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
